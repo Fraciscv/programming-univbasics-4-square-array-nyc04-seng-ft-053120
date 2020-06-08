b2828c28563e2ecf32f519c54d42f72508212534
Francisco Andaur <francisco.andaur@icloud.com>
2020-06-08 18:56:16 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
-def square_array(array)
-  # your code here
+require 'pry'
+
+def square_array(num)
+  index = 0
+  new_array = []
+
+  while index < num.length do
+    new_array << num[index] ** 2
+    index += 1
+  end
+  new_array
 end
